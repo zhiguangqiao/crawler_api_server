26f7de09970ba0994bf18d2b8006ea76aca04d47
qiaozhiguang <860638830@qq.com>
2019-04-05 18:36:32 +0800

    migrate,创建秘钥

---
+++ rsa_private_key.rb
@@ -0,0 +1,24 @@
+def my_pri_key
+    <<~HEREDOC
+        -----BEGIN RSA PRIVATE KEY-----
+        MIICXQIBAAKBgQDKqhqBThTNu5qhzUM2CZ+62Kc46Logw3f4HF+6KpLZHw6bn+tz
+        VhylgKLblw7Epv03OZFF1+eb6eudX+i1o32ZBdBgsNR1/h7uTkN0y2jJbmBIFqbc
+        /t5ZR2VlewQNNtzIzAiayrtVGYkaQtiWL3lpR0XvRScwXHHz5lCH3EOicQIDAQAB
+        AoGBAKqEJtrmvGBBWaCLmOJNhKRsB369aWWJsVYDYZO8oGOoWt0b17nqGR4zNlN2
+        LInPSrv9ZrqSJyAq2ABIov64+UGYaHlG6uruvCLWkZ7CyokvtBGx/KFO2FYLaQby
+        F2Nn+aSl6g0dJohnudF2C55S9XNZqYwG8ZB3bzqdzRzlZ3GxAkEA7OWdvFi/bIn4
+        hkuJJxvDRnv0Pkg6+K4QTomxcjhmWLfG3p9kPw12wX4C0D/myGmx3JLquZliMoiO
+        bMH98VdGRQJBANsBzyBPFFjYBAkQwdB3B+G8WQ3Ro5wYuWGGujCgunB/v+8sBKxk
+        k8I3A8gzpnZ7DF4LPkweZgd7t+VCEjf4lD0CQFJ5HhEUVJBU+FDIu1cCM1m2jbGh
+        M+afss2k4kIBoKACWxnmI+BPr6htVk1p8c6T/XftLgqve6BcKjly0BXVbIECQQDL
+        /vpkuORWARh5LT+rI0SXMq6s3YoNW0ZKHomNxBkFsykf6huMTL8JV99Q+zNiBym0
+        67oiguo8rDPV+14m/odtAkAcYGPMS/cylZv7jXh0e2Tlx45/8vbC2T4AhXBwRx41
+        cP/Y5VpdAhFCFwGooPBYdn9yaVzx55aQiXkv3RTqfr04
+        -----END RSA PRIVATE KEY-----
+    HEREDOC
+end
+
+
+
+
+
---
+++ rsa_public_key.rb
@@ -0,0 +1,10 @@
+def my_pub_key
+    <<~HEREDOC
+        -----BEGIN PUBLIC KEY-----
+        MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDKqhqBThTNu5qhzUM2CZ+62Kc4
+        6Logw3f4HF+6KpLZHw6bn+tzVhylgKLblw7Epv03OZFF1+eb6eudX+i1o32ZBdBg
+        sNR1/h7uTkN0y2jJbmBIFqbc/t5ZR2VlewQNNtzIzAiayrtVGYkaQtiWL3lpR0Xv
+        RScwXHHz5lCH3EOicQIDAQAB
+        -----END PUBLIC KEY-----
+    HEREDOC
+end
